784a75bb0b1296f8af3266e4edbbd15b4c0366ec
Dtheran <dtheran1@gmail.com>
2022-11-22 21:49:46 -0500

    lint

--- tests/unit/components/qr_code_input.spec.js
+++ tests/unit/components/qr_code_input.spec.js
@@ -1,35 +1,35 @@
-import { shallowMount } from "@vue/test-utils"
-import QRCodeInput from '@/components/QRCodeInput.vue';
-import { createStore } from "vuex";
+import { shallowMount } from '@vue/test-utils'
+import QRCodeInput from '@/components/QRCodeInput.vue'
+import { createStore } from 'vuex'
 
 describe('QRCodeInput.vue', () => {
-    describe('mouting a component', () => {
-        it('renders qur code input component', () => {
-            const wrapper = shallowMount(QRCodeInput);
+  describe('mouting a component', () => {
+    it('renders qur code input component', () => {
+      const wrapper = shallowMount(QRCodeInput)
 
-            const component = wrapper.find('.hello')
+      const component = wrapper.find('.hello')
 
-            expect(component.classes()).toContain('hello')
-        })
+      expect(component.classes()).toContain('hello')
+    })
+  })
+
+  describe('renders qr code input component cwith its dependecies', () => {
+    const store = createStore({
+      state () {
+        return { count: 1 }
+      }
     })
 
-    describe('renders qr code input component cwith its dependecies', () => {
-        const store = createStore({
-            state() {
-                return { count: 1 }
-            }
-        })
-
-        const wrapper = shallowMount(QRCodeInput,
-            {
-                global:
+    const wrapper = shallowMount(QRCodeInput,
+      {
+        global:
                     { plugin: [store] }
-            })
+      })
 
-        it('renders generate qr code button with test', () => {
-            const component = wrapper.find('#btn-generate')
+    it('renders generate qr code button with test', () => {
+      const component = wrapper.find('#btn-generate')
 
-            expect(component.text()).toBe('Generar QR')
-        })
+      expect(component.text()).toBe('Generar QR')
     })
+  })
 })
