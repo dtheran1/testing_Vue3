fff287d1eea0574c2146c95a2197e6fa28d8b346
Dtheran <dtheran1@gmail.com>
2022-11-22 21:48:46 -0500

    init

---
+++ tests/unit/components/qr_code_input.spec.js
@@ -0,0 +1,35 @@
+import { shallowMount } from "@vue/test-utils"
+import QRCodeInput from '@/components/QRCodeInput.vue';
+import { createStore } from "vuex";
+
+describe('QRCodeInput.vue', () => {
+    describe('mouting a component', () => {
+        it('renders qur code input component', () => {
+            const wrapper = shallowMount(QRCodeInput);
+
+            const component = wrapper.find('.hello')
+
+            expect(component.classes()).toContain('hello')
+        })
+    })
+
+    describe('renders qr code input component cwith its dependecies', () => {
+        const store = createStore({
+            state() {
+                return { count: 1 }
+            }
+        })
+
+        const wrapper = shallowMount(QRCodeInput,
+            {
+                global:
+                    { plugin: [store] }
+            })
+
+        it('renders generate qr code button with test', () => {
+            const component = wrapper.find('#btn-generate')
+
+            expect(component.text()).toBe('Generar QR')
+        })
+    })
+})
